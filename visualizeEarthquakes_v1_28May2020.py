7204eaca5744ec66db25729eaf812aad6190b32d
Miguel Imperial <63440740+miguel123-gis@users.noreply.github.com>
2020-05-28 21:31:26 +0800

    Update visualizeEarthquakes_v1_28May2020.py

--- visualizeEarthquakes_v1_28May2020.py
+++ visualizeEarthquakes_v1_28May2020.py
@@ -1,42 +1,31 @@
 import os
 os.chdir(r'path')
 import matplotlib.pyplot as plt
-from matplotlib.transforms import offset_copy
 import pandas as pd
 import geopandas as gpd
 from geopandas import GeoDataFrame
-import cartopy.crs as ccrs
-import cartopy.io.img_tiles as cimgt
+from shapely.geometry import Point
+os.environ['PROJ_LIB'] = r'C:\Users\imper\miniconda3\envs\eq\Library\share\basemap'
+from mpl_toolkits.basemap import Basemap
 
-df = pd.read_csv('emscPhilippines2008to2020.csv') 
-crs = "epsg:32651" 
-geometry = gpd.points_from_xy(df.Longitude, df.Latitude)
-gdf = GeoDataFrame(df, crs=crs, geometry=geometry) 
-proj = ccrs.PlateCarree() 
-ph = gpd.read_file('Provinces.shp') 
-bounds = [117.17427453, 126.537423944, 5.58100332277, 18.5052273625] 
-stamen_terrain = cimgt.Stamen('terrain-background')
-for i in range(gdf.shape[0]):
-    ax = plt.axes(projection=proj)
-    ax.add_image(stamen_terrain, 8)
-    ax.set_extent(bounds) 
-    
-    g = gdf.iloc[i].geometry
-    ax.plot(g.x, g.y, marker='o', color='red', markersize=15) 
-    
-    date = gdf.iloc[i]['Date'] 
-    time = gdf.iloc[i]['Time_UTC'] 
-    mag = gdf.iloc[i]['Magnitude'] 
-    info = (date + " " + time + " " + str("Magnitude:") + str(mag))
-    plt.suptitle('Earthquakes in the Philippines from 2008 to 2020') 
-    plt.title(info) 
-    
-    plt.show()
-    #plt.savefig("earthquake{0}.png".format(i))
-plt.show()
-
-# NEXT STEPS as of 28 May 2020
-# convert to GIF from via moviepy
-# need to use csv module isntead of geopandas
-# widen bounds
-# for-loop should start from earliest (last row in csv)
+def plotPoint():
+    df = pd.read_csv('emscPhilippines2008to2020_5lines.csv')
+    prov  = gpd.read_file('Provinces.shp')
+    crs = "epsg:32651"
+    geometry = gpd.points_from_xy(df.Longitude, df.Latitude)
+    gdf = GeoDataFrame(df, crs=crs, geometry=geometry)
+    for i in range(gdf.shape[0]): 
+        ax = prov.plot(figsize=(15,10)) 
+        ax.axis('off')
+        g = gdf.iloc[i].geometry
+        date = gdf.iloc[i]['Date']
+        time = gdf.iloc[i]['Time_UTC']   
+        mag = gdf.iloc[i]['Magnitude'] 
+        info = (date + " " + time + " "+ str("Magnitude:") + str(mag))
+        plt.plot(g.x, g.y, marker='o', color='red', markersize=15)
+        plt.suptitle('Earthquakes in the Philippines from 2008 to 2020')
+        plt.title(info)
+        plt.savefig("exports\eqph5_28May2020\earthquake{0}.png".format(i))
+        
+plotPoint()
+# still needs to convert images to gif via moviepy or other software
